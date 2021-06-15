1eeaa9cdba69def83db314d9086a84b3984f61de
Ty Mick <ty@tymick.me>
2021-06-15 08:43:19 -0700

    Add checkpoint redirect

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/checkpoint_1.1",
+        destination:
+          "https://github.com/PacktPublishing/Architecting-Enterprise-React-Applications-with-Hooks/blob/Checkpoint_1.1/components/TodoInput.jsx#L5-L13",
+        permanent: true,
+      },
+    ];
+  },
+};

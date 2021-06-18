e943d4ad194bb49fcc14081e1c2544afa46a5ef2
Ty Mick <ty@tymick.me>
2021-06-18 07:57:27 -0700

    Add Checkpoint 1.2

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Being written by [Ty Mick](https://tymick.me), coming 2022 from [Packt Publishin
 ### Chapter 1 – State: Remembering the Past
 
 - [Checkpoint 1.1](https://github.com/PacktPublishing/Architecting-Enterprise-React-Applications-with-Hooks/blob/Checkpoint_1.1/components/TodoInput.jsx#L5-L13)
+- [Checkpoint 1.2](https://github.com/PacktPublishing/Architecting-Enterprise-React-Applications-with-Hooks/blob/Checkpoint_1.2/components/TodoInput.jsx#L5-L10)
--- next.config.js
+++ next.config.js
@@ -7,6 +7,12 @@ module.exports = {
           "https://github.com/PacktPublishing/Architecting-Enterprise-React-Applications-with-Hooks/blob/Checkpoint_1.1/components/TodoInput.jsx#L5-L13",
         permanent: true,
       },
+      {
+        source: "/checkpoint_1.2",
+        destination:
+          "https://github.com/PacktPublishing/Architecting-Enterprise-React-Applications-with-Hooks/blob/Checkpoint_1.2/components/TodoInput.jsx#L5-L10",
+        permanent: true,
+      },
     ];
   },
 };

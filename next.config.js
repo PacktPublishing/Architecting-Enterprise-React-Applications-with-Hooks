d1bf1bd6a6147b9982a56bc7efa7d792c638d85b
Ty Mick <ty@tymick.me>
2021-06-18 08:02:14 -0700

    Use hyphens in checkpoint redirect URLs

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: "/checkpoint_1.1",
+        source: "/checkpoint-1.1",
         destination:
           "https://github.com/PacktPublishing/Architecting-Enterprise-React-Applications-with-Hooks/blob/Checkpoint_1.1/components/TodoInput.jsx#L5-L13",
         permanent: true,
       },
       {
-        source: "/checkpoint_1.2",
+        source: "/checkpoint-1.2",
         destination:
           "https://github.com/PacktPublishing/Architecting-Enterprise-React-Applications-with-Hooks/blob/Checkpoint_1.2/components/TodoInput.jsx#L5-L10",
         permanent: true,

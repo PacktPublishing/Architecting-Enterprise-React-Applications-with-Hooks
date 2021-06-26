aae8fe8365abccce3636e488913891bab5c37c74
Ty Mick <ty@tymick.me>
2021-06-26 12:14:13 -0700

    Add checkpoint rebase bubble script

---
+++ scripts/checkpoint-rebase-bubble.js
@@ -0,0 +1,53 @@
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+(async function () {
+  try {
+    const branchList = (await exec("git branch --list")).stdout;
+    const checkpointBranches = [
+      ...branchList.matchAll(/Checkpoint_(?<chapter>\d+)\.(?<decimal>\d+)/g),
+    ].map(([name, chapter, decimal]) => ({
+      name,
+      chapter: parseInt(chapter),
+      decimal: parseInt(decimal),
+    }));
+    checkpointBranches.sort(
+      (
+        { chapter: chapterA, decimal: decimalA },
+        { chapter: chapterB, decimal: decimalB }
+      ) => {
+        if (chapterA === chapterB) return decimalA - decimalB;
+        else return chapterA - chapterB;
+      }
+    );
+    const branches = [...checkpointBranches.map(({ name }) => name), "main"];
+
+    const startingBranch = (
+      await exec("git branch --show-current")
+    ).stdout.trimEnd();
+
+    let startingBranchReached = false;
+    let previousBranch = "";
+    for (const branch of branches) {
+      if (startingBranchReached === false) {
+        if (branch === startingBranch) {
+          startingBranchReached = true;
+          await exec("git push --force-with-lease");
+          previousBranch = branch;
+        }
+
+        continue;
+      }
+
+      await exec(`
+        git checkout ${branch};
+        git rebase --strategy-option ours ${previousBranch};
+        git push --force-with-lease;
+      `);
+      previousBranch = branch;
+    }
+  } catch (error) {
+    console.error(error.stderr);
+    process.exitCode = 1;
+  }
+})();

082ca3def6375f348862047181a56cf006807e62
Ty Mick <ty@tymick.me>
2021-06-29 07:40:27 -0700

    Add Checkpoint 1.14

--- next.config.js
+++ next.config.js
@@ -79,6 +79,12 @@ module.exports = {
           "https://github.com/PacktPublishing/Architecting-Enterprise-React-Applications-with-Hooks/compare/Checkpoint_1.13%5E..Checkpoint_1.13",
         permanent: true,
       },
+      {
+        source: "/checkpoint-1.14",
+        destination:
+          "https://github.com/PacktPublishing/Architecting-Enterprise-React-Applications-with-Hooks/blob/Checkpoint_1.14/components/LanguageSelect.jsx",
+        permanent: true,
+      },
     ];
   },
 };

42bee6c01f754e7141f60d53e820782f4ed30e2d
Ty Mick <ty@tymick.me>
2021-07-03 14:21:05 -0700

    Add to-do list reducer

---
+++ reducers/todoListReducer.js
@@ -0,0 +1,38 @@
+import { OrderedMap, Record } from "immutable";
+import { nanoid } from "nanoid";
+
+const Task = Record({ description: "", complete: false });
+
+export const initialTasks = OrderedMap({
+  [nanoid()]: Task({ description: "Learn how to use `useReducer`." }),
+});
+
+export const TASK_ACTIONS = Object.freeze({
+  ADD: "add",
+  TOGGLE_COMPLETE: "toggle-complete",
+  DELETE: "delete",
+  DELETE_ALL_COMPLETE: "delete-all-complete",
+});
+
+export default function todoListReducer(prevTasks, action) {
+  const { ADD, TOGGLE_COMPLETE, DELETE, DELETE_ALL_COMPLETE } = TASK_ACTIONS;
+
+  switch (action.type) {
+    case ADD:
+      return prevTasks.set(nanoid(), Task({ description: action.description }));
+
+    case TOGGLE_COMPLETE:
+      return prevTasks.updateIn([action.key], (task) =>
+        task.set("complete", !task.complete)
+      );
+
+    case DELETE:
+      return prevTasks.delete(action.key);
+
+    case DELETE_ALL_COMPLETE:
+      return prevTasks.filter((task) => !task.complete);
+
+    default:
+      throw new Error(`'${action.type}' is not a valid action type.`);
+  }
+}

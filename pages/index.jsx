52d5d90dcbaa8aba1e0c7768f1e06349698bcfbd
Ty Mick <ty@tymick.me>
2021-09-13 09:09:42 -0700

    Adjust component lifecycle console logs

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,24 +8,24 @@ import DeleteAllCompleteButton from "../components/DeleteAllCompleteButton";
 import todoListReducer, { initialTasks } from "../reducers/todoListReducer";
 import TodoListDispatch from "../contexts/TodoListDispatch";
 
+let renderCount = 0;
+
 export default function Home() {
-  console.log("Starting render");
+  console.log(`Render #${++renderCount}`);
+
   const { localizedStrings } = useContext(LocalizationContext);
 
   const [tasks, dispatch] = useReducer(todoListReducer, initialTasks);
 
   useEffect(() => {
-    console.log("Starting page title effect");
+    console.log("Running page title effect");
 
     const incompleteTaskCount = tasks.filter((task) => !task.complete).size;
     document.title = `(${incompleteTaskCount}) ${localizedStrings.projectTitle}`;
-
-    console.log("Ending page title effect");
   }, [tasks, localizedStrings.projectTitle]);
 
   const [switchState, setSwitchState] = useState(false);
 
-  console.log("Ending render");
   return (
     <>
       <LanguageSelect />

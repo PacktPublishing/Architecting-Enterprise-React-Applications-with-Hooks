ee9d8844255cebf46ba0a8f4bb10d0318b53f7db
Ty Mick <ty@tymick.me>
2021-09-22 07:15:26 -0700

    Remove meaningless switch

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import Link from "next/link";
-import { Container, Form } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { subscribeToTaskList } from "../models/database";
 import { LocalizationContext } from "../contexts/localization";
 import LanguageSelect from "../components/LanguageSelect";
@@ -14,8 +14,6 @@ export default function Home() {
   console.log(`Render #${++renderCount}`);
 
   const { localizedStrings } = useContext(LocalizationContext);
-
-  const [switchState, setSwitchState] = useState(false);
 
   const [tasks, setTasks] = useState([]);
 
@@ -52,13 +50,6 @@ export default function Home() {
         <Link href="/about">
           <a className="d-inline-block mb-3">About page link</a>
         </Link>
-        <Form.Check
-          type="switch"
-          id="meaningless-switch"
-          label="Meaningless switch"
-          checked={switchState}
-          onChange={() => setSwitchState((s) => !s)}
-        />
       </div>
 
       <Container fluid style={{ maxWidth: "720px" }} className="mt-5 mb-4">

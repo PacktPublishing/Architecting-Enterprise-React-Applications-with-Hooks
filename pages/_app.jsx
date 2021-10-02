28fbdd28d018cee1c6d7091bea69f0106cc8123b
Ty Mick <ty@tymick.me>
2021-10-02 13:54:36 -0700

    Import dynamically and disable SSR

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,14 @@
 import React from "react";
-import { LocalizationProvider } from "../contexts/localization";
+import dynamic from "next/dynamic";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const LocalizationProvider = dynamic(
+  () =>
+    import("../contexts/localization").then(
+      (module) => module.LocalizationProvider
+    ),
+  { ssr: false }
+);
 
 export default function App({ Component, pageProps }) {
   return (

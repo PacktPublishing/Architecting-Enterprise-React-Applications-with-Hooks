7174b22b8cd4d0ee752cef8039719af9117162f2
Ty Mick <ty@tymick.me>
2021-10-02 12:24:47 -0700

    Revert "Detect locale on the server side instead"

--- contexts/localization.jsx
+++ contexts/localization.jsx
@@ -1,13 +1,30 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useLayoutEffect, useState } from "react";
 import localization from "../localization.json";
 
 const supportedLocales = Object.keys(localization);
-const DEFAULT_LOCALE = "tlh";
 
 export const LocalizationContext = createContext();
 
-export function LocalizationProvider({ initialLocale, children }) {
-  const [locale, setLocale] = useState(initialLocale);
+export function LocalizationProvider({ children }) {
+  const [locale, setLocale] = useState("tlh");
+
+  useLayoutEffect(() => {
+    const userLocales = (navigator?.languages ?? [navigator?.language]).filter(
+      (language) => language != null
+    );
+
+    for (const userLocale of userLocales) {
+      const userLanguage = userLocale.split("-")[0];
+      const resolvedLocale =
+        supportedLocales.find((locale) => locale === userLocale) ??
+        supportedLocales.find((locale) => locale.startsWith(userLanguage));
+
+      if (resolvedLocale) {
+        setLocale(resolvedLocale);
+        break;
+      }
+    }
+  }, []);
 
   return (
     <LocalizationContext.Provider
@@ -20,24 +37,4 @@ export function LocalizationProvider({ initialLocale, children }) {
       {children}
     </LocalizationContext.Provider>
   );
-}
-
-export function getInitialLocale(appContext) {
-  const acceptLanguage = appContext.ctx.req.headers["accept-language"];
-  const userLocales = acceptLanguage
-    .split(",")
-    .map((locale) => locale.split(";")[0].trim());
-
-  for (const userLocale of userLocales) {
-    const userLanguage = userLocale.split("-")[0];
-    const resolvedLocale =
-      supportedLocales.find((locale) => locale === userLocale) ??
-      supportedLocales.find((locale) => locale.startsWith(userLanguage));
-
-    if (resolvedLocale) {
-      return resolvedLocale;
-    }
-  }
-
-  return DEFAULT_LOCALE;
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,23 +1,11 @@
 import React from "react";
-import NextApp from "next/app";
-import {
-  getInitialLocale,
-  LocalizationProvider,
-} from "../contexts/localization";
+import { LocalizationProvider } from "../contexts/localization";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function App({ initialLocale, Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
-    <LocalizationProvider initialLocale={initialLocale}>
+    <LocalizationProvider>
       <Component {...pageProps} />
     </LocalizationProvider>
   );
 }
-
-App.getInitialProps = async (appContext) => {
-  const appPropsTask = NextApp.getInitialProps(appContext);
-  return {
-    initialLocale: getInitialLocale(appContext),
-    ...(await appPropsTask),
-  };
-};

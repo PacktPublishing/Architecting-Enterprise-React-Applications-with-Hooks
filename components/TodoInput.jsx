f79c7dfd18b64ea4f220e2b6dcf501ba03968ddb
Ty Mick <ty@tymick.me>
2021-11-10 07:20:16 -0800

    Remove input-saving effect
    It complicates the upcoming example.

--- components/TodoInput.jsx
+++ components/TodoInput.jsx
@@ -1,33 +1,12 @@
-import React, { useContext, useLayoutEffect, useRef } from "react";
+import React, { useContext, useRef } from "react";
 import { Col, Button, Form, Row } from "react-bootstrap";
 import { LocalizationContext } from "../contexts/localization";
 import { addTask as dbAddTask } from "../models/database";
-import { SAVED_INPUT } from "../models/localStorage-keys";
 
 export default function TodoInput(props) {
   const localizedStrings = useContext(LocalizationContext);
 
   const inputRef = useRef(null);
-
-  useLayoutEffect(() => {
-    const inputElement = inputRef.current;
-
-    const savedInputValue = localStorage.getItem(SAVED_INPUT);
-    if (savedInputValue) {
-      inputElement.value = savedInputValue;
-      localStorage.removeItem(SAVED_INPUT);
-    }
-
-    function saveInput() {
-      if (inputElement.value)
-        localStorage.setItem(SAVED_INPUT, inputElement.value);
-    }
-    window.addEventListener("beforeunload", saveInput);
-    return function cleanup() {
-      saveInput();
-      window.removeEventListener("beforeunload", saveInput);
-    };
-  }, []);
 
   async function addTask() {
     if (inputRef.current.value !== "") {
